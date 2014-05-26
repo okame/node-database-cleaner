f7a4fe1245dcc1d01b81fe0a003d8ff14d569125
okame <pikkoroo888@gmail.com>
2014-05-26 22:23:17 +0900

    Appropreate mysql connection pool

--- test/mysql.test.js
+++ test/mysql.test.js
@@ -6,53 +6,82 @@ var config = require('./mysql.config');
 
 var mysql = require('mysql'), 
 client = mysql.createConnection(config);
+pool_client = mysql.createPool(config);
 database = client.config.database;
 
 describe('mysql', function() {
     beforeEach(function(done) {
-        client.query('CREATE DATABASE IF NOT EXISTS ' + database, function(err, res) {
-            client.query('USE ' + database, function(err, res) {
-                client.config.database = database;
-                client.query('CREATE TABLE test1 (id INTEGER NOT NULL AUTO_INCREMENT, title VARCHAR(255) NOT NULL, PRIMARY KEY(id));', function(err, res) {
+        prepareData(done);
+    });
+
+    afterEach(function(done) {
+        dropData(done);
+    });
+
+    describe('client', function() {
+        it('should delete all records', function(done) {
+            databaseCleaner.clean(client, function(err, res) {
+                if(err) console.log(err);
+                client.query("SELECT * FROM test1", function(err, result_test1) {
                     if(err) console.log(err);
-                        client.query('CREATE TABLE test2 (id INTEGER NOT NULL AUTO_INCREMENT, title VARCHAR(255) NOT NULL, PRIMARY KEY(id));', function(err, res) {
-                            if(err) console.log(err);
-                                client.query('INSERT INTO test1(title) VALUES(?)', ["foobar"], function(err, res) {
-                                    if(err) console.log(err);
-                                        client.query('INSERT INTO test2(title) VALUES(?)', ["foobar"], function(err, res){
-                                            if(err) console.log(err);
-                                                done();
-                                        });
-                                });
-                        });
+                    client.query("SELECT * FROM test2", function(err, result_test2) {
+                        if(err) console.log(err);
+                        result_test1.length.should.equal(0);
+                        result_test2.length.should.equal(0);
+                        done();
+                    });
                 });
             });
         });
     });
 
-    afterEach(function(done) {
-        client.query("DROP TABLE IF EXISTS test1", function(err) {
-            if(err) console.log(err);
-            client.query("DROP TABLE IF EXISTS test2", function(err) {
+    describe('pool client', function() {
+        it('should delete all records', function(done) {
+            databaseCleaner.clean(pool_client, function(err, res) {
                 if(err) console.log(err);
-                client.end();
-                done();
-            });
-        });
-    });
-
-    it('should delete all records', function(done) {
-        databaseCleaner.clean(client, function() {
-            client.query("SELECT * FROM test1", function(err, result_test1) {
-                if(err) console.log(err);
-                client.query("SELECT * FROM test2", function(err, result_test2) {
-                    if(err) console.log(err);
-                    result_test1.length.should.equal(0);
-                    result_test2.length.should.equal(0);
-                    done();
+                pool_client.query("SELECT * FROM test1", function(err, result_test1) {
+                    pool_client.query("SELECT * FROM test2", function(err, result_test2) {
+                        result_test1.length.should.equal(0);
+                        result_test2.length.should.equal(0);
+                        pool_client.end();
+                        done();
+                    });
                 });
             });
         });
     });
 
 });
+
+function prepareData(done) {
+    client.query('CREATE DATABASE IF NOT EXISTS ' + database, function(err, res) {
+        if(err) console.log(err);
+        client.query('USE ' + database, function(err, res) {
+            if(err) console.log(err);
+            client.config.database = database;
+            client.query('CREATE TABLE test1 (id INTEGER NOT NULL AUTO_INCREMENT, title VARCHAR(255) NOT NULL, PRIMARY KEY(id));', function(err, res) {
+                if(err) console.log(err);
+                client.query('CREATE TABLE test2 (id INTEGER NOT NULL AUTO_INCREMENT, title VARCHAR(255) NOT NULL, PRIMARY KEY(id));', function(err, res) {
+                    if(err) console.log(err);
+                    client.query('INSERT INTO test1(title) VALUES(?)', ["foobar"], function(err, res) {
+                        if(err) console.log(err);
+                        client.query('INSERT INTO test2(title) VALUES(?)', ["foobar"], function(err, res){
+                            if(err) console.log(err);
+                            done();
+                        });
+                    });
+                });
+            });
+        });
+    });
+}
+
+function dropData(done) {
+    client.query("DROP TABLE IF EXISTS test1", function(err) {
+        if(err) console.log(err);
+        client.query("DROP TABLE IF EXISTS test2", function(err) {
+            if(err) console.log(err);
+            done();
+        });
+    });
+}
